65935fb4fbc729c015dc319b4aea7f7fc4203da5
FAT YODA <shivangsingh654@gmail.com>
2024-06-30 11:36:21 +0530

    hello

---
+++ script.js
@@ -0,0 +1,20 @@
+const v = document.querySelector('#v');
+let value = '';
+
+function calculate(e) {
+    value += e.innerText;
+
+    v.value += e.innerText;
+
+    if (v.value == "Infinity")
+        v.value = "";
+}
+
+function displayResult() {
+    v.value = eval(value);
+    value = "";
+}
+
+function c() {
+    v.value = "";
+}
